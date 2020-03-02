e3a42b9ba64b5852cafdf78bd854e7afb30cf96a
Nikola Cubelic <ncubelic@gmail.com>
2020-03-02 18:26:24 +0100

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Nikola on 2.3.2020..
+//  Copyright © 2020 Nikola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
